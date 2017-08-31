c608a057af3c2df4037883be0a142a710fe2e160
Pavel Zhuravlev <Pzhuravlev@econometricainc.com>
2017-08-31 19:23:49 -0400

    Forming table displaying data with jQuery instead of D3

--- js/TableView.js
+++ js/TableView.js
@@ -14,34 +14,41 @@ BDSVis.TableView = {
 		//All the values of returned xvars
 		var xvarvalues = data.map(function(d) {return d[xvar]}).filter(function(d,i,a) {return i===a.indexOf(d)})
 		//Data as table output via D3
-		var datashowtable = d3.select("#graphdata");
-		datashowtable.selectAll("*").remove()
+		var datashowtable = $("#graphdata");
+		datashowtable.children().remove();
 
-		var headers=d3.merge([[vm.model.NameLookUp(xvar,"var")],cvarvalues.map(function(d){return vm.model.NameLookUp(d,cvar)})]);
+		var headers=[].concat.apply([],[[vm.model.NameLookUp(xvar,"var")],cvarvalues.map(function(d){return vm.model.NameLookUp(d,cvar)})]);
 
-		datashowtable.append("thead") //Headers on top
-			.selectAll("th").data(headers)
-			.enter().append("th").text(function(d){return d});
+		datashowtable.append("<thead></thead>");
+		var dsthead = datashowtable.children().last();
+		$('#graphdataheaders').children().remove(); //Headers on the bottom
+		$('#graphdataheaders').append("<thead></thead>")
+		var dstbott = $('#graphdataheaders').children().last();
 
-		d3.select('#graphdataheaders').selectAll('*').remove(); //Headers on the bottom
-		d3.select('#graphdataheaders').append("thead")
-			.selectAll("th").data(headers)
-			.enter().append("th").text(function(d){return d});
+		headers.forEach(function(d){
+			dsthead.append("<th></th>");
+			dsthead.children().last().text(d);
+			dstbott.append("<th></th>");
+			dstbott.children().last().text(d);
+		})
+		
+		datashowtable.append("<tbody></tbody>");
+		var dstbody = datashowtable.children().last();
 
-		datashowtable.append("tbody")
-			.selectAll("tr").data(xvarvalues.map(function(xv){
-				return data.filter(function(d) {return d[xvar]===xv;} //Map a row of yvar values to each xvar value
-			)}))
-			.enter().append("tr")
-				.selectAll("td")
-				.data(function(dxv) {
-					return d3.merge([ [vm.model.NameLookUp(dxv[0][xvar],xvar)], //Add the xvar value as a first element of the row
+		xvarvalues.map(function(xv){return data.filter(function(d) {return d[xvar]===xv;})}).forEach(function(dxv) {
+			dstbody.append("<tr></tr>")
+			var row = dstbody.children().last();
+			var table = [].concat.apply([],[ //Merging arrays into one
+									[vm.model.NameLookUp(dxv[0][xvar],xvar)], //Add the xvar value as a first element of the row
 									cvarvalues.map(function(cv){ //Map a yvar value to each cvar/xvar values pair (or, a column of yvar values to each cvar value)
 										return dxv.filter(function(d) {return d[cvar]===cv})
 												.map(function(d) {return d.value});
 									})])
-					}).enter().append("td")
-				.text(function(d) {return d});
+			table.forEach(function(d){
+				row.append("<td></td>")
+				row.children().last().text(d)
+			})
+		})
 		
 		d3.select('#graphdata').selectAll('tr').style("background-color",function(d,i) {return (i%2)?"#fff":"#eee";});
 		this.SetLowerHeadersWidth();
